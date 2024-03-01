a26e642e0fc946d6812f226b2bb1e24336a8d803
THANHPHONGVO\vthan <THANHPHONGVO>
2024-03-02 01:22:01 +0700

    adding .env file

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,15 +1,17 @@
-// ignore_for_file: constant_identifier_names
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Constants {
   // App Fonts
   static const String app_font_DM_Sans = 'DM Sans';
 
   // App API Endpoints
-  static const String api_base_url = 'http://192.168.1.11:3111';
+  static String api_base_url = '${dotenv.env['API_BASE_URL']}';
   // Authentication API
-  static const String api_login = '$api_base_url/api/auth/login';
-  static const String api_register = '$api_base_url/api/auth/register';
-  static const String api_refresh_token =
+  static final String api_login = '$api_base_url/api/auth/login';
+  static final String api_register = '$api_base_url/api/auth/register';
+  static final String api_refresh_token =
       '$api_base_url/api/auth/refresh-token';
 
   // Constant String
--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,12 @@ import 'package:cleaning_duty_project/feature/routers/route.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MainApp());
