f6f05c385c9ca983491a8088baee7551add80aab
THANHPHONGVO\vthan <THANHPHONGVO>
2024-03-24 18:34:39 +0700

    Dialog

--- lib/feature/screen/home/home.dart
+++ lib/feature/screen/home/home.dart
@@ -13,6 +13,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
+import 'package:panara_dialogs/panara_dialogs.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -133,43 +134,82 @@ _buidlBottomSheet(BuildContext context) {
       ),
     ),
     body: Container(
+      alignment: Alignment.topCenter,
       height: double.infinity,
       color: AppColor.colorAppBar,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _buildIconButton(
-                  OMIcons.person,
-                  AppColor.colorE06D06,
-                  'Profile',
-                  () {
-                    context.read<HomeBloc>().resetState(context);
-                    context.push(ScreenRoute.profileScreen);
-                  },
-                ),
-                _buildIconButton(
-                  OMIcons.personAdd,
-                  AppColor.color219653,
-                  'Add Duty',
-                  () {
-                    context.read<HomeBloc>().resetState(context);
-                    context.push(ScreenRoute.cleanningDutyScreen);
-                  },
-                ),
-                _buildIconButton(
-                  Icons.logout_outlined,
-                  AppColor.colorEB5757,
-                  'Logout',
-                  () {
-                    context.read<LogoutBloc>().handleLogout(context);
-                  },
-                ),
-              ],
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildIconButton(
+                    OMIcons.person,
+                    AppColor.colorE06D06,
+                    'Profile',
+                    () {
+                      context.read<HomeBloc>().resetState(context);
+                      context.push(ScreenRoute.profileScreen);
+                    },
+                  ),
+                  _buildIconButton(
+                    OMIcons.personAdd,
+                    AppColor.color219653,
+                    'Add Duty',
+                    () {
+                      context.read<HomeBloc>().resetState(context);
+                      context.push(ScreenRoute.cleanningDutyScreen);
+                    },
+                  ),
+                  _buildIconButton(
+                    OMIcons.assignmentInd,
+                    AppColor.color9B51E0,
+                    'Quick Add ',
+                    () {
+                      PanaraConfirmDialog.showAnimatedGrow(context,
+                          message:
+                              'Tự Động sắp lịch trực nhật cho tháng hiện tại',
+                          confirmButtonText: 'Ok',
+                          cancelButtonText: 'Hủy', onTapConfirm: () {
+                        context.pop();
+                        context.read<HomeBloc>().resetState(context);
+                        Future.delayed(const Duration(milliseconds: 1000), () {
+                          PanaraInfoDialog.show(
+                            context,
+                            title: "Thành Công",
+                            message: "Đã sắp lịch trực nhật cho tháng hiện tại",
+                            buttonText: "Ok",
+                            onTapDismiss: () {
+                              Navigator.pop(context);
+                            },
+                            panaraDialogType: PanaraDialogType.success,
+                          );
+                        });
+                      }, onTapCancel: () {
+                        context.pop();
+                      }, panaraDialogType: PanaraDialogType.normal);
+                    },
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildIconButton(
+                    Icons.logout_outlined,
+                    AppColor.colorEB5757,
+                    'Logout',
+                    () {
+                      context.read<LogoutBloc>().handleLogout(context);
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     ),
@@ -182,39 +222,48 @@ Widget _buildIconButton(
   String text,
   VoidCallback onPressed,
 ) {
-  return Column(
+  return Stack(
+    alignment: Alignment.center,
     children: [
-      Stack(
-        alignment: Alignment.center,
+      Column(
         children: [
-          Container(
-            width: 52,
-            height: 52,
-            decoration: ShapeDecoration(
-              color: AppColor.colorWhite,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 52,
+                height: 52,
+                decoration: ShapeDecoration(
+                  color: AppColor.colorWhite,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
               ),
-            ),
+              IconButton(
+                onPressed: onPressed,
+                icon: Icon(
+                  icon,
+                  size: 40.sp,
+                  color: color,
+                  weight: 100,
+                ),
+              ),
+            ],
           ),
-          IconButton(
-            onPressed: onPressed,
-            icon: Icon(
-              icon,
-              size: 40.sp,
-              color: color,
-              weight: 100,
-            ),
-          ),
+          SizedBox(height: 20.h)
         ],
       ),
-      Text(
-        text,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 15.sp,
-          fontWeight: FontWeight.w700,
-          fontFamily: Constants.app_font_Lato,
+      Positioned(
+        bottom: 0,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 11.sp,
+            fontWeight: FontWeight.w700,
+            fontFamily: Constants.app_font_Lato,
+          ),
         ),
       ),
     ],
